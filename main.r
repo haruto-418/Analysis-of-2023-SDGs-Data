98f9dd7c81af5a05e4606c56647e6a5a2b9b0f19
KikuchiHaruto <kikuchi@amunzen.com>
2023-12-04 19:15:53 +0900

    Add correlation analysis for SDG goals

--- main.r
+++ main.r
@@ -36,7 +36,7 @@ plot(sdgs_index_score, percentage_missing_values, xlab="SDGs Index Score", ylab=
 plot(sdgs_index_score, international_spillovers_score, xlab="SDGs Index Score", ylab="International Spillovers Score") # nolint
 # -> 若干相関がありそう。
 
-# 3. 1~17のゴールごとのスコアとの相関を見る
+# 3. 1~17のゴールごとのスコアとの相関を見る（散布図を出力）
 for (i in 1:17) {
   # ファイル名を設定
   file_name <- paste0("output/SDG_Goal_", i, "_Score_Plot.png")
@@ -56,3 +56,29 @@ for (i in 1:17) {
   # デバイスを閉じる（ファイルを保存）
   dev.off()
 }
+
+# 4. 1~17のゴールごとのスコアとの相関を見る（相関係数を出力）
+
+correlation_coefficients <- c()
+
+for (i in 1:17) {
+  # カラム名を動的に生成
+  column_name <- paste0("Goal.", i, ".Score")
+
+  # 対応するSDGスコアを抽出
+  goal_score <- data[[column_name]][1:193]
+
+  # 相関係数を出力
+  correlation_coefficient <- cor(sdgs_index_score, goal_score, use = "complete.obs") # nolint
+
+  # 相関係数を配列に追加
+  correlation_coefficients <- c(correlation_coefficients, correlation_coefficient) # nolint
+}
+
+# 相関係数を出力
+correlation_coefficients
+
+# 各ゴールごとの相関係数を棒グラフで出力し、棒グラフをPNGファイルとして出力
+png("output/SDG_Goal_Correlation_Coefficient_Plot.png")
+barplot(correlation_coefficients, names.arg = 1:17, xlab = "SDG Goal", ylab = "Correlation coefficient") # nolint
+dev.off()
